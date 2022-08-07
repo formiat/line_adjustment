5665376cbc5fc92583188d259cd7ad31f9f9f7b7
Alexander Aglerimov <formiat95@gmail.com>
2022-08-08 00:42:14 +0400

    optimization and code clean

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+// 100 spaces
+const SPACES: &str = "                                                                                                    ";
+
 /// No check of word length, i.e., `line_width` must be greater or equal to every word len.
 pub fn transform(input: &str, output_line_len: usize) -> String {
     let mut lines = Vec::with_capacity(input.chars().count() / 4);
@@ -7,31 +10,34 @@ pub fn transform(input: &str, output_line_len: usize) -> String {
     let mut last_line_len = 0;
 
     if let Some(first_word) = words_iter.next() {
-        last_line_parts.push(first_word);
-        last_line_len = last_line_parts[0].len();
+        if !first_word.is_empty() {
+            last_line_parts.push(first_word);
+            last_line_len = last_line_parts[0].len();
 
-        for new_word in words_iter {
-            // In theory: `min_spaces_count = (last_line_parts.len() - 1) + 1`
-            let min_spaces_count = last_line_parts.len();
-            let new_word_chars_count = new_word.chars().count();
+            for new_word in words_iter {
+                // In theory: `min_spaces_count = (last_line_parts.len() - 1) + 1`
+                let min_spaces_count = last_line_parts.len();
+                let new_word_chars_count = new_word.chars().count();
 
-            if last_line_len + min_spaces_count + new_word_chars_count > output_line_len {
-                // `last_line_parts` is full. There we transform it to a string.
+                if last_line_len + min_spaces_count + new_word_chars_count > output_line_len {
+                    // `last_line_parts` is full. There we transform it to a string.
 
-                let new_line =
-                    process_last_line_parts(&last_line_parts, last_line_len, output_line_len);
-                lines.push(new_line);
+                    let new_line =
+                        process_last_line_parts(&last_line_parts, last_line_len, output_line_len);
+                    lines.push(new_line);
 
-                last_line_parts.clear();
-                last_line_len = 0;
+                    last_line_parts.clear();
+                    last_line_len = 0;
+                }
+
+                last_line_parts.push(new_word);
+                last_line_len += new_word_chars_count;
             }
 
-            last_line_parts.push(new_word);
-            last_line_len += new_word_chars_count;
+            let new_line =
+                process_last_line_parts(&last_line_parts, last_line_len, output_line_len);
+            lines.push(new_line);
         }
-
-        let new_line = process_last_line_parts(&last_line_parts, last_line_len, output_line_len);
-        lines.push(new_line);
     }
 
     lines.join("\n")
@@ -43,32 +49,29 @@ fn process_last_line_parts(
     output_line_len: usize,
 ) -> String {
     let space_chars_count = output_line_len - last_line_len;
-    println!("space_chars_count: {}", space_chars_count);
 
     if last_line_parts.len() == 1 {
-        let spaces = format!("{:width$}", "", width = space_chars_count);
+        let spaces = &SPACES[..space_chars_count];
 
         format!("{}{}", last_line_parts[0], spaces)
     } else {
         let gaps_count = last_line_parts.len() - 1;
-        println!("gaps_count: {}", gaps_count);
 
         // The compiler will optimize it so that only a single instruction is emitted
         let (space_chars_per_gap, extra_space_chars_count) = (
             space_chars_count / gaps_count,
             space_chars_count % gaps_count,
         );
-        println!("space_chars_per_gap: {}", space_chars_per_gap);
-        let spaces = format!("{:width$}", "", width = space_chars_per_gap);
+        let spaces = &SPACES[..space_chars_per_gap];
 
         let mut new_line = String::with_capacity(output_line_len);
 
         for (i, last_line_part) in last_line_parts.iter().enumerate() {
             new_line.push_str(last_line_part);
 
-            // If last
+            // If is not last
             if i < last_line_parts.len() - 1 {
-                new_line.push_str(&spaces);
+                new_line.push_str(spaces);
             }
 
             // If is needed to add extra space
@@ -88,14 +91,14 @@ mod tests {
     #[test]
     fn simple() {
         let test_cases = [
-            // ("", 5, ""),
-            // ("test", 5, "test "),
+            ("", 5, ""),
+            ("test", 5, "test "),
             ("Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua", 12,
              "Lorem  ipsum\ndolor    sit\namet        \nconsectetur \nadipiscing  \nelit  sed do\neiusmod     \ntempor      \nincididunt  \nut labore et\ndolore magna\naliqua      ")
         ];
 
         for &(input, line_width, expected) in &test_cases {
-            // println!("input: '{:#?}'", input.split(' ').collect::<Vec<_>>());
+            println!("input: '{:#?}'", input.split(' ').collect::<Vec<_>>());
             let output = transform(input, line_width);
             println!("output: '{:#?}'", output.split('\n').collect::<Vec<_>>());
             println!(
